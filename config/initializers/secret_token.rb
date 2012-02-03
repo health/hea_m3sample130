f9530383e032fef4ceceae95c73f2fbea52adf35
health <w21winner@gmail.com>
2012-02-03 18:36:53 +0200

    m3sample130 03022012

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+M3sample130::Application.config.secret_token = 'c8068daeafd0408b0d4d6ebcbac5d21ba5f9519f5d835ff4d3e2d9a51781ab469dc9ee8ae1303a70bde55cd43db63389859a995e69c8d992e8edb263736cc5bd'
